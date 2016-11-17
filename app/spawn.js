8eeada9ce0ccacac3143fa26f2dcd96975f17d17
Senio Caires <seniocaires@gmail.com>
2016-11-17 21:08:56 -0200

    spawn

---
+++ app/spawn.js
@@ -0,0 +1,11 @@
+var spawn = require('child_process').spawn;
+var command = spawn('git', ['push', 'origin', 'master']);
+command.stdout.on('data', function (data) {
+  console.log('stdout: ' + data);
+});
+command.stderr.on('data', function (data) {
+  console.log('stderr: ' + data);
+});
+command.on('close', function (code) {
+  console.log('child process exited with code ' + code);
+});
