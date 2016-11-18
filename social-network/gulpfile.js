4fec78bc18138f52d65d7f1002a9b8e697ac5f26
Senio Caires <seniocaires@gmail.com>
2016-11-18 15:13:26 -0200

    gulp-css

---
+++ social-network/gulpfile.js
@@ -0,0 +1,22 @@
+var path = require('path');
+var gulp = require('gulp');
+var less = require('gulp-less');
+var rename = require("gulp-rename");
+var minifyCSS = require('gulp-minify-css');
+
+gulp.task('css', function() {
+  gulp.src('./less/styles.less')
+  .pipe(less({
+    paths: [ path.join(__dirname, 'less', 'includes') ]
+  }))
+  .pipe(gulp.dest('./static/css'))
+  .pipe(minifyCSS({keepBreaks:true}))
+  .pipe(rename({suffix: '.min'}))
+  .pipe(gulp.dest('./static/css'));
+});
+
+gulp.task('watchers', function() {
+  gulp.watch('less/**/*.less', ['css']);
+});
+
+gulp.task('default', ['css', 'watchers']);
